57f02961dd0216d1186613a2343980df67c58f2c
git-zenghw <zhw15159517501@163.com>
2026-05-22 23:29:27 +0800

    调整Makefile, 完成基本main函数编写

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * @Author: zenghw
+ * @Date: 2026-05-22 23:21:24
+ * @Description: 
+ * @LastEditTime: 2026-05-22 23:25:49
+ * Copyright (c) 2023 by 汇创智控科技有限公司, All Rights Reserved
+ */
+
+ #include <stdio.h>
+ #include <stdint.h>
+
+
+ int main(int argc, char *argv[])
+ {
+
+    printf("linux c skills demo\n");
+    return 0;
+ }
